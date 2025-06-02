b6474336e51685187cba4bb7544c4d607fbafd09
Marcelo Wendel <mwbc.cs@gmail.com>
2025-06-02 15:48:16 -0300

    Listando por ordem decrescente de cadastro de lancamentos.

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -21,7 +21,7 @@ export class EntryListComponent implements OnInit{
 
     this.entryService.getAll().subscribe({
 
-      next: entries => this.entries = entries, 
+      next: entries => this.entries = entries.sort((a, b) => b.id! - a.id!), 
       error: () => alert('Erro ao carregar a lista de lançamentos')
     });
   }
