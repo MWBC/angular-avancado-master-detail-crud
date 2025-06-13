06cea18699cb877e2c2d2a56d4c4618855d4f1ca
Marcelo Wendel <mwbc.cs@gmail.com>
2025-06-12 21:03:16 -0300

    Adicionando template para relatorios.

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent {
 
+  breadCrumbLinks: Array<any> = [];
+
+  pageTitle: string;
+
+  constructor() {
+
+    this.breadCrumbLinks = [
+
+      {
+
+        'text': 'Relatório de Receitas e Despesas'
+      }
+    ];
+
+    this.pageTitle = 'Relatório de Receitas e Despesas';
+  }
+
+  generateReports(){}
 }
