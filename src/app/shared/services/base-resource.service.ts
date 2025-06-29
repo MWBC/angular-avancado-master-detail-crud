7b316c84befb34c876f4e83f5fec426f53cec3a9
Marcelo Wendel <mwbc.cs@gmail.com>
2025-06-28 21:39:15 -0300

    Corrigindo bug na url do metodo de delete no base-resource service.

--- src/app/shared/services/base-resource.service.ts
+++ src/app/shared/services/base-resource.service.ts
@@ -60,7 +60,7 @@ export abstract class BaseResourceService<T extends BaseResourceModel> {
  
     delete(id: number): Observable<any> {
     
-        const url = `${this.apiPath}/${id}}`;
+        const url = `${this.apiPath}/${id}`;
     
         return this.http.delete(url).pipe(
     
