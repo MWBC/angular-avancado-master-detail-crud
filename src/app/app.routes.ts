5f0657536482c7abcecd9a22a1ad82636346f990
Marcelo Wendel <mwbc.cs@gmail.com>
2025-07-19 22:38:04 -0300

    Implementando rota de not found.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { LoginLayoutComponent } from './shared/components/login-layout/login-lay
 import { AppLayoutComponent } from './shared/components/app-layout/app-layout.component';
 import { authGuard } from './auth.guard';
 import { LogoutComponent } from './core/components/logout/logout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
 
-    {path: '', component: LoginLayoutComponent, children: [{path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)}, {path: '', redirectTo: '/reports', pathMatch: 'full'}]}, 
+    {path: '', component: LoginLayoutComponent, children: [
+        {path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)}, {path: '', redirectTo: '/reports', pathMatch: 'full'}
+    ]}, 
     {path: '', component: AppLayoutComponent, children: [
 
         {path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule), canActivate: [authGuard]}, 
@@ -15,5 +18,6 @@ export const routes: Routes = [
         {path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule), canActivate: [authGuard]}, 
         {path: 'logout', component: LogoutComponent}, 
         {path: '', redirectTo: '/login', pathMatch: 'full'}
-    ]}
+    ]}, 
+    {path: '**', component: NotFoundComponent}
 ];
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 
-import { CookieService } from 'ngx-cookie-service';
-
 @NgModule({
   declarations: [],
   imports: [
